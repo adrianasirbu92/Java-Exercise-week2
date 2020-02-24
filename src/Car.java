5db063c769c4e1924bb3d68151797b8d981212fa
adrianasirbu92 <56941428+adrianasirbu92@users.noreply.github.com>
2020-02-24 15:48:31 -0500

    week 3 homework 2

---
+++ src/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+        private String country;
+        private String model;
+        private String maker;
+
+    public void setCountry(String a) {
+        country=a;
+    }
+    public void setModel (String b){
+        model=b;
+    }
+
+    public void setMaker(String c) {
+        maker=c;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+}
